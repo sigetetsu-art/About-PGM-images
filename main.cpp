7acea73fda857295e954c0c118669db7e0a90244
sigetetsu-art <66481711+sigetetsu-art@users.noreply.github.com>
2021-12-19 02:39:52 +0900

    Update and rename ソース.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -22,30 +22,39 @@ Image::Image() {
 
 
 int main(int ac, char *av[]) {
-	FILE *fp;
+	FILE* fp, *fo;
 	char tmp[256];
 	Image img;
-	unsigned int x, y;
 
 	fp = fopen(av[1], "rb");
 	if (fp == NULL) {
-		cout << "入力ファイルがありません" << endl;
+		cout << "蜈･蜉帙ヵ繧｡繧､繝ｫ縺後≠繧翫∪縺帙ｓ" << endl;
 		exit(8);
 	}
+	fo = fopen(av[2], "wb");
+	if (fo == NULL) {
+		cout << "蜃ｺ蜉帙ヵ繧｡繧､繝ｫ縺後≠繧翫∪縺帙ｓ" << endl;
+		exit(8);
+	}
+
 	fgets(tmp, 256, fp);
+	fprintf(fo, "P5\n");
+	
 	cout << "Magic Number = ";
 	cout << tmp[0];
 	cout << tmp[1] << endl;
 
 	fgets(tmp, 256, fp);
-	sscanf(tmp,"%d %d",&img.width, &img.height);
-	cout <<"Image width = "<< img.width << endl;
-	cout<< "Image height = " << img.height << endl;
+	sscanf(tmp, "%d %d", &img.width, &img.height);
+	cout << "Image width = " << img.width << endl;
+	cout << "Image height = " << img.height << endl;
 	vector<vector<unsigned char>> value(img.height, vector<unsigned char>(img.width));
 
 	fgets(tmp, 256, fp);
 	sscanf(tmp, "%d", &img.max_value);
 	cout << "Maximum brightness value = " << img.max_value << endl << endl;
+
+	fprintf(fo, "%d %d\n%d\n", img.width, img.height, img.max_value);
 
 	for (int y = 0; y < img.height; y++) {
 		for (int x = 0; x < img.width; x++) {
@@ -53,11 +62,20 @@ int main(int ac, char *av[]) {
 		}
 	}
 
-	cout << "取得したい輝度値の座標を入力してください" << endl;
-	cin >> x >> y;
-	cout << "(x,y) = (" << x << ", " << y << ") 座標の輝度値は" << (int)value[y][x] << endl;
+	for (int y = 0; y < img.height; y++) {
+		for (int x = 0; x < img.width; x++) {
+			if (value[y][x] * 2 > 255) {
+				value[y][x] = 255;
+			}
+			else {
+				value[y][x] = value[y][x] * 2;
+			}
+			fputc(value[y][x], fo);
+		}
+	}
 
 	fclose(fp);
+	fclose(fo);
 
 	return 0;
 }
